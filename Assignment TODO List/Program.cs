e2ab2e3d9aa38eace676ceac69d8320059a06b05
Mikecolmey313@gmail.com <Mikecolmey313@gmail.com>
2024-09-24 19:56:21 -0400

    created a while loop that uses a boolean variable that is only set to true if the user selects option e (exit)
    set bool shallExit = false and while !shallExit code block for user menu and switch statement is exectued in a loop until user selects the Exit option, changing the boolean variable shallExit to true and terminating the loop

--- Assignment TODO List/Program.cs
+++ Assignment TODO List/Program.cs
@@ -1,33 +1,37 @@
 ﻿Console.WriteLine("Hello!");
-Console.WriteLine("What do you want to do?");
-Console.WriteLine("[S]ee all todos");
-Console.WriteLine("[A]dd a todo");
-Console.WriteLine("[R]emove a todo");
-Console.WriteLine("[E]xit");
 
-var userChoice = Console.ReadLine();
-
-switch(userChoice)
+bool shallExit = false;
+while (!shallExit)
 {
-    case "s":
-    case "S":
-        Console.WriteLine("See all TODOs");
-        break;
-    case "a":
-    case "A":
-        Console.WriteLine("Add a TODO");
-        break;
-    case "r":
-    case "R":
-        Console.WriteLine("Remove a TODO");
-        break;
-    case "e":
-    case "E":
-        Console.WriteLine("Exit");
-        break;
-    default:
-        Console.WriteLine("Invalid choice");
-        break;
-}
+    Console.WriteLine("What do you want to do?");
+    Console.WriteLine("[S]ee all todos");
+    Console.WriteLine("[A]dd a todo");
+    Console.WriteLine("[R]emove a todo");
+    Console.WriteLine("[E]xit");
 
+    var userChoice = Console.ReadLine();
+
+    switch (userChoice)
+    {
+        case "s":
+        case "S":
+            Console.WriteLine("See all TODOs");
+            break;
+        case "a":
+        case "A":
+            Console.WriteLine("Add a TODO");
+            break;
+        case "r":
+        case "R":
+            Console.WriteLine("Remove a TODO");
+            break;
+        case "e":
+        case "E":
+            shallExit = true;
+            break;
+        default:
+            Console.WriteLine("Invalid choice");
+            break;
+    }
+}
 Console.ReadKey();
